dfbccbf0eea0e9ccd03c5154249bb7e0aa6a80e6
Matheus Garcia <matheusgarciajbs@gmail.com>
2023-09-29 22:21:19 -0300

    script frase

--- js/script.js
+++ js/script.js
@@ -9,3 +9,35 @@ function redirect() {
   window.open(url, "_blank")
 }
 
+
+
+const title = document.getElementById('title-two')
+
+function digitacao() {
+  setTimeout(() => {
+    title.innerHTML = 'realça o seu potencial!'
+    title.style.width = '0ch'
+    title.style.animation = 'frase2 5s steps(23), pisca 0.5s infinite step-end alternate'
+  }, 6000);
+    
+  setTimeout(() => {
+    title.innerHTML = 'eleva seu profissionalismo!'
+    title.style.width = '0ch'
+    title.style.animation = 'frase3 6s steps(27), pisca 0.5s infinite step-end alternate'
+  }, 12000);
+  
+  setTimeout(() => {
+    title.innerHTML = 'te valoriza!'
+    title.style.width = '0ch'
+    title.style.animation = 'frase4 4s steps(12), pisca 0.5s infinite step-end alternate'
+  }, 19000);
+
+  setInterval(() => {
+    title.innerHTML = 'destaca seu negócio!'
+    title.style.width = '0ch'
+    title.style.animation = 'frase1 5s steps(23), pisca 0.5s infinite step-end alternate'
+  }, 25000); 
+}
+
+digitacao()
+setInterval(digitacao, 25000)
