977f6ff4af163213e3ae2ffe9f840f158ed21b56
Matheus Garcia <matheusgarciajbs@gmail.com>
2023-12-28 22:26:00 -0300

    msg wpp link

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ const footerText = document.querySelector('.year')
 footerText.innerHTML = year
 
 function redirect() {
-  const url = 'https://wa.me/+5512974033681?text=Ol%C3%A1%21+Vim+atrav%C3%A9s+de+seu+site+e+gostaria+de+fazer+um%C2%A0or%C3%A7amento.'
+  const url = 'https://wa.me/+5512996760918'
   window.open(url, "_blank")
 }
 
